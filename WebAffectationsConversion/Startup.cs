4acc5c0506f1826309cbe8838fd905bfca7917d1
SIO2 <SIO2@SIO2-PC>
2016-09-19 19:35:31 +0200

    Ajoutez des fichiers projet.

---
+++ WebAffectationsConversion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAffectationsConversion.Startup))]
+namespace WebAffectationsConversion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
